f5b25fa9cb3046cea8c2982def2eedda67331d7f
Altynbek312 <altyn_312@mail.ru>
2023-06-09 11:57:55 +0300

    feat: make button disabled while fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import StartModal from "./components/Modal/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button } from "react-bootstrap";
 import { wordsActions } from "./store/store";
-import { useAppDispatch } from "./hooks/typedStoreHooks";
+import { useAppDispatch, useAppSelector } from "./hooks/typedStoreHooks";
 import Text from "./components/Text/Text";
 
 function App() {
   const dispatch = useAppDispatch();
-
+  const isLoading = useAppSelector((state) => state.isLoading);
   return (
     <>
       <Container className="d-flex flex-column align-items-center mb-3">
@@ -16,6 +16,7 @@ function App() {
         <Results />
         <Text />
         <Button
+          disabled={isLoading}
           className="mt-3 fs-5"
           onClick={() => {
             dispatch(wordsActions.resetApp());
